3614f2917cac1c3ea2c37d99b805aa74b430a984
dwleonhardt <dwleonhardt@gmail.com>
2017-08-31 15:59:25 -0600

    bootstrap test/basic splash page

--- pauline/src/components/Home.js
+++ pauline/src/components/Home.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
-
+import {Jumbotron, Button, Col } from 'react-bootstrap'
+import Navigation from './Nav'
 
 class Home extends Component {
   render() {
     return (
-      <h1>CHECK OUT Home</h1>
+      <div>
+        <Navigation />
+        <Col md={8} mdOffset={2} className="container">
+          <Jumbotron>
+            <h1>Welcome to Pauline</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p><Button bsStyle="primary">Learn more</Button></p>
+          </Jumbotron>
+        </Col>
+      </div>
     );
   }
 }
---
+++ pauline/src/components/Nav.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Navbar, Nav, NavItem } from 'react-bootstrap'
+
+class Navigation extends Component {
+  render() {
+    return (
+      <Navbar>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="#">Pauline</a>
+          </Navbar.Brand>
+        </Navbar.Header>
+        <Nav>
+          <NavItem eventKey={1} href="#">About</NavItem>
+          <NavItem eventKey={2} href="#">Contact</NavItem>
+        </Nav>
+        <Nav pullRight>
+          <NavItem eventKey={3} href="#">Login</NavItem>
+        </Nav>
+
+      </Navbar>
+    );
+  }
+}
+
+export default Navigation;
